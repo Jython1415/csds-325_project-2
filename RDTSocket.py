35851719a539c2b66a447036c7a0cc39cfd61fe8
Joshua Shew <joshua.t.shew@gmail.com>
2023-03-30 21:49:12 -0400

    RDTSocket set up

--- RDTSocket.py
+++ RDTSocket.py
@@ -1,18 +1,25 @@
+import utility as Utility
 
-
-class RDTSocket:
+class RDTSocket(Utility.UnreliableSocket):
     
-    def accept():
-        print("accept")
+    def __init__(self, ip, port):
+        Utility.UnreliableSocket.__init__(self, ip, port) 
+        self.sendAddress = None 
+        
+    def accept(self):
+        (socket, address) = self.socket.accept()[0]
+        self.socket = socket
+        return address
     
-    def connect():
-        print("connect")
+    def connect(self, address):
+        self.socket.connect(address)
+        self.sendAddress = address
     
-    def send():
-        print("send")
+    def send(self, data):
+        self.sendto(data, self.sendAddress)
     
-    def recv():
-        print("recv")
+    def recv(self):
+        return self.recvfrom(2048)
     
-    def close():
-        print("close")
+    def close(self):
+        self.close()
