9f6b815da0a2c794e897fb35a7bbc5b524311233
Joshua Shew <joshua.t.shew@gmail.com>
2023-04-01 11:12:12 -0400

    removed unnecessary code

--- utility.py
+++ utility.py
@@ -6,35 +6,36 @@ class UnreliableSocket:
     
     def __init__(self, ip = None, port = None):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.messageQueue = []
-        self.delayedMessage = None
-        self.connected = False
+        self.messageQueue = []     # Used to store an internal list of packets for delaying and reordering them
+        self.delayedMessage = None # Used to store a delayed message
         
         self.address = (ip, port)
+        # Bind the socket if it has an address to bind to
         if ip != None:
             self.bind()
     
+    # Used to bind the socket to a port
     def bind(self):
         self.socket.bind(self.address)
     
     # Returns the results in the format (Packet, address)
     # If there is nothing to return, it returns (None, None)
     def recvfrom(self, bufferSize):
-        # Handle delayed message (if there is one)
-        if self.delayedMessage != None:
-            self.messageQueue.append(self.delayedMessage)
-            self.delayedMessage = None
-        
         # Receive new data
         try:
             newMessage = self.socket.recvfrom(bufferSize, socket.MSG_DONTWAIT)
         except:
             newMessage = (None, None)
         (packetData, _) = newMessage
-        if packetData != None: # <- check new data and append it to the queue
+        if packetData != None:
             self.messageQueue.append(newMessage)
         
-        # If there is nothing in the queue
+        # Handle delayed message (if there is one)
+        if self.delayedMessage != None:
+            self.messageQueue.append(self.delayedMessage)
+            self.delayedMessage = None
+        
+        # If there is nothing in the queue, return nothing
         if len(self.messageQueue) == 0:
             return (None, None)
         
@@ -60,27 +61,27 @@ class UnreliableSocket:
                         encodedText[selectedByte] = encodedText[selectedByte] ^ 5 # Modify the byte
                         packet.text = encodedText.decode("utf-8")                 # Switch the data to the modified data
 
-        # Reformat the object
-        returnTuple = (packet, currentMessage[1])
-
         # Return the message
+        returnTuple = (packet, currentMessage[1])
         return returnTuple
-                    
+    
+    # Used to send a packet       
     def sendto(self, packet, address):
+        # Encode package
         packet.packetHeader.address = self.socket.getsockname()
         data = pickle.dumps(packet)
+        # Check for packages that are too long
         if len(data) > 1400:
             raise Exception(f"data size too big: {len(data)} > 1400")
         
-        # if not self.connected:
-        #     self.socket.connect(address)
-        #     self.connected = True
+        # Send
         try:
             self.socket.send(data)
-        except:
+        except: # If there is an error, connect the socket first
             self.socket.connect(address)
             self.socket.send(data)
     
+    # Close the socket
     def close(self):
         self.socket.close()
         
@@ -129,6 +130,10 @@ class Packet:
         newPacket.packetHeader = PacketHeader(3, seq_num, 0, newPacket.compute_checksum())
         return newPacket
     
+    @staticmethod
+    def getSeqNum(packet) -> int:
+        return packet.packetHeader.seq_num
+    
     def compute_checksum(self) -> int:
         if self.text != None:
             return zlib.crc32(self.text.encode())
@@ -137,9 +142,41 @@ class Packet:
     
     def verify_packet(self) -> bool:
         return self.packetHeader.checksum == self.compute_checksum()
+
+    # Get the estimated size of the package when it will be sent
+    def compressedSize(self) -> int:
+        return len(pickle.dumps(self))
     
     def __eq__(self, obj):
         if type(obj) != type(self):
             return False
         else:
             return self.packetHeader == obj.packetHeader and self.text == obj.text
+        
+    # Used to see if the package is in the input list
+    def isInList(self, list):
+        found = False
+        for obj in list:
+            if obj == self:
+                found = True
+        return found
+
+class Debug:
+    
+    # Fills in text will an input fill character to a certain length
+    # The fill character is added to the left of the input text
+    @staticmethod
+    def leftFill(inputText, targetLength, fillCharacter = " ") -> str:
+        if len(inputText) > targetLength:
+            raise Exception(f"Debug.leftFill: Input text is too long\n{len(inputText)} > {targetLength}")
+        else:
+            return (fillCharacter * (targetLength - len(inputText))) + inputText
+       
+    # Fills in text will an input fill character to a certain length
+    # The fill character is added to the right of the input text 
+    @staticmethod
+    def rightFill(inputText, targetLength, fillCharacter = " ") -> str:
+        if len(inputText) > targetLength:
+            raise Exception(f"Debug.leftFill: Input text is too long\n{len(inputText)} > {targetLength}")
+        else:
+            return inputText + (fillCharacter * (targetLength - len(inputText)))
